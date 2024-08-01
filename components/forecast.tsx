fa96e48905de679daaf643cab8a40968ffd90002
Hayk Karyan <hayk.karyan@milies.net>
2024-08-01 12:43:25 +0400

    fixed console warnings

--- components/forecast.tsx
+++ components/forecast.tsx
@@ -3,9 +3,9 @@ import { format } from 'date-fns';
 export const Forecast = ({ currentData }: any) =>
   <div className="p-20">
     <h2 className="text-xl font-medium">5-day forecast</h2>
-    {currentData.timestamp.map((e: any) => {
+    {currentData.timestamp.map((e: any, i: number) => {
       return (
-        <div className='min-w-80 flex justify-between'>
+        <div key={i} className='min-w-80 flex justify-between'>
           <p>{format(e.dt_txt, 'E, LLL d')}</p>
           <p>{e.main.temp_min} / {e.main.temp_max}°C</p>
           <p className='text-yellow-600'>{e.weather[0].description}</p>
